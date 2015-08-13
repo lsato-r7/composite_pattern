8ea9780eba4fce94bab9808fb4e7cea0d3b544a2
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-08-13 12:27:43 -0500

    add CompositePatter::Field

--- lib/composite_pattern/field.rb
+++ lib/composite_pattern/field.rb
@@ -1,3 +1,7 @@
 class CompositePattern::Field
+  attr_accessor :n_bits
 
+  def initialize(n_bits: nil)
+    @n_bits = n_bits
+  end
 end
--- spec/lib/field_spec.rb
+++ spec/lib/field_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 RSpec.describe CompositePattern::Field do
+  describe '#n_bits' do
+    context 'when n_bits is NOT specified for .new' do
+      it 'n_bits is set to nil' do
+        field = CompositePattern::Field.new
+        expect(field.n_bits).to be_nil
+      end
+    end
+
+    context 'when n_bits is specified for .new' do
+      it 'n_bits is set to nil' do
+        field = CompositePattern::Field.new(n_bits: 64)
+        expect(field.n_bits).to eql 64
+      end
+    end
+  end
 end
