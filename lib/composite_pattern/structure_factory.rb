a5221880446a3dd83c1e1c878aa3b9829c0412ae
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-08-14 22:33:55 -0500

    a little more better

--- lib/composite_pattern/structure_factory.rb
+++ lib/composite_pattern/structure_factory.rb
@@ -1,13 +1,11 @@
 module CompositePattern::StructureFactory
-
   class << self
     attr_accessor :endian, :field_type, :field_name
 
     def for(endian: :little, field_type:, field_name:)
-      fields = [[field_type, field_name]]
-      structure_factory = BinData::Struct.new(endian: :little, fields: fields)
-      structure_factory.new
+      structure_factory = BinData::Struct.new(endian: :little,
+                                              fields: [[field_type, field_name]])
+      packet_structure  = structure_factory.new
     end
-
   end
 end
--- spec/lib/leaf_field_spec.rb
+++ spec/lib/leaf_field_spec.rb
@@ -1,34 +1,27 @@
 require 'spec_helper'
 
-RSpec.describe CompositePattern::LeafField do
+module CompositePattern
+RSpec.describe LeafField do
   describe '#value' do
     context 'when :value keyword argument is specified' do
       it 'sets the accessor to the value specified' do
-        field = CompositePattern::LeafField.new value: 'is subjective'
+        field = LeafField.new value: 'is subjective'
         expect(field.value).to eql 'is subjective'
       end
     end
 
     context 'when :value keyword argument NOT passed with .new' do
       it 'sets the accesor to an empty string' do
-        field = CompositePattern::LeafField.new
+        field = LeafField.new
         expect(field.value).to eql ''
       end
     end
   end
 
+  describe '#structure_factory' do
+    it 'TODO: do we need to check anything here?'
+  end
 
-  # context 'when :packet_factory keyword argument is specified' do
-  #   it 'sets the accessor to the value specified' do
-  #     field = CompositePattern::LeafField.new packet_factory: 'sounds fun'
-  #     expect(field.packet_factory).to eql 'sounds fun'
-  #   end
-  # end
+end
 
-  # context 'when :packet_factory keyword argument NOT passed with .new' do
-  #   it 'sets the accesor to BinData::Struct' do
-  #     field = CompositePattern::LeafField.new
-  #     expect(field.packet_factory).to eql BinData::Struct
-  #   end
-  # end
 end
